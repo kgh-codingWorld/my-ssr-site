df10ee8ff76af80fbe297afc20ccf9eb62828d52
kgh-codingWorld <tto4028@naver.com>
2025-04-02 14:57:31 +0900

    add annotation and first commit to ver.redis-cache branch

--- server/utils/file_util.py
+++ server/utils/file_util.py
@@ -1,7 +1,9 @@
 import requests
 from server.config import MINIO_URL
 
+# MinIO 서버에서 파일을 가져오는 기능을 제공
+
 def fetch_file(filename: str) -> str:
-    res = requests.get(f"{MINIO_URL}/{filename}")
+    res = requests.get(f"{MINIO_URL}/{filename}") # MinIO 서버에서 파일을 GET 요청으로 가져옴
     res.raise_for_status()  # 실패 시 예외 발생
-    return res.text
+    return res.text # 파일의 내용을 텍스트로 반환
